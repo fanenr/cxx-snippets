030e2f1c76decf4a324c9b8b4a6448012a15a20f
Arthur63 <Arthur63@163.com>
2024-01-31 14:36:25 +0800

    update 24-01-31

--- test/test.cpp
+++ test/test.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
-#include <memory>
 
-// void pfree(int* ptr) noexcept
-// {
-//     delete ptr;
-// }
-
-struct Test {
-    int a;
-    double b;
-};
-
-int main()
+int
+main ()
 {
-    auto pa = new Test();
-    std::unique_ptr<Test> upa(pa);
-    delete pa;
-    auto pb = new Test();
-    // std::unique_ptr<Test> upb(pb);
-    // pfree(pa);
+  std::cout << "hello world!\n";
 }
